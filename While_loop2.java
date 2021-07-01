3ae726c6cd3f78b7fcbb7e9b738f15c05ba913e1
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-07-01 19:43:30 +0530

    Add files via upload

---
+++ While_loop2.java
@@ -0,0 +1,12 @@
+package cond;
+public class While_loop2 {
+	public static void main(String[] args) {
+		int n = 5, i = 1;
+		while (i<=10)
+		{
+		  System.out.println(n + " X " + i +" = "+ (n*i));
+		  i++;
+		}
+       System.out.println("end of the loop");
+	}
+}
