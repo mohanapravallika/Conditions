773904d4cbb2586475c6bfad5d659466cb305143
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-07-01 19:44:17 +0530

    Add files via upload

---
+++ Do_while.java
@@ -0,0 +1,12 @@
+package cond;
+public class Do_while {
+	public static void main(String[] args) {
+		int n = 5, i = 1;
+		do
+		{
+		System.out.println(n + " X " + i +" = "+ (n*i));
+		i++;
+		}while(i<=10);
+		System.out.println("End of the loop");
+	}
+}
