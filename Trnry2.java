040c4c63379278e14e0969268287f28f1e34210a
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-07-03 22:14:42 +0530

    Add files via upload

---
+++ Trnry2.java
@@ -0,0 +1,13 @@
+package cond;
+import java.util.*;
+public class Trnry2 
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Enter two numbers : ");
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    int c = (a>b) ? a : b ;
+	System.out.println("bigger number = "+ c);
+    }
+}
