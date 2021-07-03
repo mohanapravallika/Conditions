a63ee0fc3e8d14daf3946873d705c7316b349a9d
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2021-07-03 22:15:09 +0530

    Add files via upload

---
+++ Pos_negtve.java
@@ -0,0 +1,12 @@
+package cond;
+public class Pos_Negtve {
+	public static void main(String[] args) {
+		int n = -1821;
+		if(n > 0)
+			System.out.println("positive integer");
+		else if (n < 0)
+			System.out.println("negitive integer");
+		else
+			System.out.println("Zero");
+	}
+}
